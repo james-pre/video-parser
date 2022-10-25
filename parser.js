3031fc87cef639cb0bc696fafcb8c78ac133147a
Dr. Vortex <vortex.doc@gmail.com>
2022-10-25 09:54:06 -0500

    Fixes

--- parser.js
+++ parser.js
@@ -1,92 +1,93 @@
 class VideoParser {
-	#video = document.createElement('video');
-	#canvas = document.createElement('canvas');
-	#render = document.createElement('canvas');
-	#context;
-	#renderContext;
+	_video = document.createElement('video');
+	_canvas = document.createElement('canvas');
+	_render = document.createElement('canvas');
+	_context;
+	_renderContext;
 
-	constructor(file, video, render){
-		if(!(file instanceof File)) throw new TypeError(`Can't parse non-files`);
-		if(!(this.#video.canPlayType(file.type))) throw new TypeError(`Can't parse file of type ${file.type}`);
+	constructor(file, video, render) {
+		if (!(file instanceof File)) throw new TypeError(`Can't parse non-files`);
+		if (!this._video.canPlayType(file.type)) throw new TypeError(`Can't parse file of type ${file.type}`);
 
-		if(video instanceof HTMLVideoElement) this.#video = video;
-		if(render instanceof HTMLCanvasElement) this.#render = render;
-	
-		this.#video.src = URL.createObjectURL(file);
-		this.#video.load();
-		this.#video.muted = true;		
+		if (video instanceof HTMLVideoElement) this._video = video;
+		if (render instanceof HTMLCanvasElement) this._render = render;
 
-		this.#video.onloadedmetadata = () => {
-			this.#canvas.width = this.#video.videoWidth;
-			this.#canvas.height = this.#video.videoHeight;
-			this.#render.width = this.#video.videoWidth;
-			this.#render.height = this.#video.videoHeight;
-			this.#video.width = this.#video.videoWidth;
-			this.#video.height = this.#video.videoHeight;
-		}
+		this._video.src = URL.createObjectURL(file);
+		this._video.load();
+		this._video.muted = true;
 
-		this.#context = this.#canvas.getContext('2d', {willReadFrequently: true});
-		this.#renderContext = this.#render.getContext('2d', {willReadFrequently: true});
+		this._video.onloadedmetadata = () => {
+			this._canvas.width = this._video.videoWidth;
+			this._canvas.height = this._video.videoHeight;
+			this._render.width = this._video.videoWidth;
+			this._render.height = this._video.videoHeight;
+			this._video.width = this._video.videoWidth;
+			this._video.height = this._video.videoHeight;
+		};
 
+		this._context = this._canvas.getContext('2d', { willReadFrequently: true });
+		this._renderContext = this._render.getContext('2d', { willReadFrequently: true });
 	}
 
-	frameData(parser){
+	frameData(parser) {
+		return new Promise((resolve) => {
+			const originalFrames = [],
+				parsedFrames = [],
+				recordedChunks = [];
 
-		return new Promise(resolve => {
-			const originalFrames = [], parsedFrames = [], recordedChunks = [];
-
-			let stream = this.#render.captureStream(0);
+			let stream = this._render.captureStream(0);
 			let recorder = new MediaRecorder(stream);
 
-			this.#video.ontimeupdate = async () => {
+			this._video.ontimeupdate = async () => {
+				await new Promise(res => setTimeout(res, 8));
 
-				await new Promise(res => setTimeout(res, 4));
-
-				this.#video.pause();
+				this._video.pause();
 
 				let frame = originalFrames.length;
 
-				if(this.#video.videoWidth > 0){
-					this.#context.drawImage(this.#video, 0, 0);
-				
-					let data = this.#context.getImageData(0, 0, this.#video.videoWidth, this.#video.videoHeight).data, parsedData;
+				if (this._video.videoWidth > 0) {
+					this._context.drawImage(this._video, 0, 0);
+
+					let data = this._context.getImageData(0, 0, this._video.videoWidth, this._video.videoHeight).data,
+						parsedData;
 
 					originalFrames.push(data);
 
-					if(typeof parser == 'function'){
-						parsedData = new ImageData(parser(data, frame), this.#video.videoWidth, this.#video.videoHeight);
-						this.#renderContext.putImageData(parsedData, 0, 0);
+					if (typeof parser == 'function') {
+						parsedData = new ImageData(parser(data, frame), this._video.videoWidth, this._video.videoHeight);
+						this._renderContext.putImageData(parsedData, 0, 0);
 						parsedFrames.push(parsedData);
-					}					
+					}
 				}
 
-				if (this.#video.currentTime < this.#video.duration) {
-					this.#video.play();
+				if (this._video.currentTime < this._video.duration) {
+					this._video.play();
 				}
 			};
 
-			recorder.ondataavailable = e => {
-				console.log(e)
+			recorder.ondataavailable = (e) => {
+				console.log(e);
 				if(e.data.size > 0){
 					recordedChunks.push(e.data);
 				}
-			}
+				
+			};
 
-			this.#video.onended = () => {
+			this._video.onended = async () => {
 				recorder.stop();
-				const blob = new Blob(parsedFrames, {type: 'video/webm'});
+				await new Promise(res => setTimeout(res, 25));
+				let blob = new Blob(recordedChunks, {type: 'video/webm;codecs=vp8'});
 				console.log(blob);
 				resolve(originalFrames, parsedFrames, blob);
-			}
+			};
 
-			this.#video.playbackRate = 2;
+			this._video.playbackRate = 2;
 			recorder.start();
-			this.#video.play();
+			this._video.play();
 		});
 	}
 
-	get videoCompletion(){
-		return this.#video.currentTime / this.#video.duration;
+	get videoCompletion() {
+		return this._video.currentTime / this._video.duration;
 	}
-
 }
